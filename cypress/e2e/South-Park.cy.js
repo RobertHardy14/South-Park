11433ce034dfc13ba675f890729de02e06bfb19b
Robert <rob.moreno@hotmail.com>
2023-06-09 17:56:53 -0600

    Added new Cases to automation
    Added Select episode from specific season and Select collection

--- cypress/e2e/South-Park.cy.js
+++ cypress/e2e/South-Park.cy.js
@@ -17,4 +17,22 @@ describe('South Park Test', () => {
         })
     })
 
+
+    it('Selects an Episode from a specific Season', () => {
+        cy.get('.menu-toggle').click()
+        cy.get('[data-testid="sub-nav-item_2"]').click()
+        cy.get('[data-display-name="Button"]').contains('Episodios Completos').click() //It got to the Complete Episodes screen
+
+        cy.get('[data-testid="Dropdown"]').scrollIntoView().click()
+        cy.get('[data-testid="DropdownList"]').within(() => {
+            cy.get('[data-display-name="Link"]').contains('Temporada 13').click() //It selected the Season
+        })
+
+        cy.get('h3').contains('El Mapache').click()
+    })
+
+    it('Selects a Collection', () => {
+        cy.get('h2').contains('Colección: Best of Cartman').click() //It enters the best of cartman page, we still need to click a link on a parragraph
+        cy.get('[data-display-name="GenericText"]').contains("Best of Cartman").click()
+    })
 })
